65a341daa4a8a3259f1a11ae66134e94f0b51111
Tichq <57302714+qTich@users.noreply.github.com>
2022-11-13 17:00:34 +0300

    add: `manage/health` for services

--- src/reservation/reservation/urls.py
+++ src/reservation/reservation/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('manage/', include('health.urls')),
     path('api/v1/', include('source.urls')),
 ]
